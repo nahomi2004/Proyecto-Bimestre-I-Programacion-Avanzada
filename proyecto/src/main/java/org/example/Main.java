3510c128ef59fe7b7040456fd83d16b21de51b37
André Flores <oaflores7@utpl.edu.ec>
2024-05-07 08:51:43 -0500

    Main

--- proyecto/src/main/java/org/example/Main.java
+++ proyecto/src/main/java/org/example/Main.java
@@ -1,9 +1,19 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("Hello world!");
+        
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Menú\nSeleccione el rol:");
+        System.out.println("Cliente             [1]");
+        System.out.println("Empleado            [2]");
+        System.out.println("Bodeguero           [3]");
+        int op = sc.nextInt();
+        
+        
     }
 
 }
