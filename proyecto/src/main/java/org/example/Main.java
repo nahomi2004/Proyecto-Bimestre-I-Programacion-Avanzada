4ecef0aeb28c049e28d976d1c64f4f323c532766
André Flores <oaflores7@utpl.edu.ec>
2024-05-07 08:36:55 -0500

    Arreglo main

--- proyecto/src/main/java/org/example/Main.java
+++ proyecto/src/main/java/org/example/Main.java
@@ -4,9 +4,6 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
-        System.out.println("hola");
-        System.out.println("Lacone");
-        System.out.println("feel it");
     }
 
 }
